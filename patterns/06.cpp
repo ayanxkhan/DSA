f7d539ec08151c05a5ae707723927e2da8376cd5
Ayan Khan <ayanxkhan12@gmail.com>
2024-02-17 15:19:53 +0530

    pattern

--- patterns/06.cpp
+++ patterns/06.cpp
@@ -1,13 +1,33 @@
-// 12345
-// 1234
-// 123
-// 12
-// 1
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+
+        // space
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+
+
+        // star
+        for(int j=0;j<2*i+1;j++){
+            cout << "*";
+        }
+
+
+
+        // space
+         for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+
+        cout << endl;
+
+    }    
     
    
     return 0;
