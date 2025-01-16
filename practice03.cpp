d888b9ef518dffcb83b026d9e8ea69086b740f91
Ayan Khan <ayanxkhan12@gmail.com>
2025-01-17 01:41:30 +0530

    `Refactored func() to take a vector and its size as arguments, added a second variable to track the second largest number, and modified the loop to update both largest and second largest numbers.`

--- practice03.cpp
+++ practice03.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int func(int num){
-    if(num<=1){
-        return num;
-    }
+int func(vector<int>&arr,int n){
+    int largest = arr[0];
+    int Slargest = -1;
 
-    return func(num-2) + func(num-1);
+    for(int i=0;i<n;i++){
+        if(arr[i] > largest){
+            Slargest= largest;
+            largest = arr[i];
+        }
+
+        if(arr[i]<largest && arr[i]>Slargest){
+            Slargest = arr[i];
+        }
+    }
+    cout << Slargest;
 }
 
 int main(){
-    cout << func(3);
+    vector<int> arr={1,66,7,4,23,87,9,122,90,3};
+    int n = arr.size();
+
+    func(arr,n);
     return 0;
 }
