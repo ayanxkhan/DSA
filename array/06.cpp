d21401a3be05f6d0a2dff1b2dae5a9313974e941
Ayan Khan <ayanxkhan12@gmail.com>
2025-02-26 03:29:09 +0530

    practice

--- array/06.cpp
+++ array/06.cpp
@@ -1,32 +1,32 @@
-// Count frequency of digits
-#include<bits/stdc++.h>
-using namespace std;
+// // Count frequency of digits
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int countFrequency(int arr[],int n,int d){
-    int cnt=0;
-    for(int i=0;i<n;i++){
-        if(arr[i] == d){
-            cnt++;
-        }
-    }
-    return cnt;
-}
+// int countFrequency(int arr[],int n,int d){
+//     int cnt=0;
+//     for(int i=0;i<n;i++){
+//         if(arr[i] == d){
+//             cnt++;
+//         }
+//     }
+//     return cnt;
+// }
 
-int main(){
-    int n;
-    cin >> n;
+// int main(){
+//     int n;
+//     cin >> n;
 
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin >> arr[i];
+//     }
 
-    int digit;
-    cin >> digit;
+//     int digit;
+//     cin >> digit;
 
-    cout << countFrequency(arr,n,digit);
-    return 0;
-}
+//     cout << countFrequency(arr,n,digit);
+//     return 0;
+// }
 
 
 
